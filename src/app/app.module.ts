039890fc736d3300ca46c684932b1ea73a5f9ce4
Abdullahabid3691 <abdullahabid3691.com>
2021-09-16 00:17:59 +0500

    Mock Http Server with InMemoryDb
    Allow fetching and updating Resource from InMemoryDb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './services/in-memory-data.service';
+
 import {HeroesComponent} from "./components/heroes/heroes.component";
 import {HeroDetailComponent} from "./components/heroes/hero-detail/hero-detail.component";
 import {MessagesComponent} from "./components/messages/messages.component";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -20,9 +24,14 @@ import {DashboardComponent} from "./components/dashboard/dashboard.component";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -2,23 +2,58 @@ import {Injectable} from '@angular/core';
 import {Observable, of} from "rxjs";
 import {Hero} from "../interfaces/hero";
 import {MessageService} from "./message.service";
-import {HEROES} from "../interfaces/mock-heros";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {catchError, tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  apiEndpoint = 'api/heroes'
 
-  constructor(private messageService: MessageService) {
+  constructor(private http: HttpClient,
+              private messageService: MessageService) {
+
+  }
+
+  logMessage(message: string): void {
+    this.messageService.add(message);
   }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add("Fetching Heroes from server....")
-    return of(HEROES);
+    this.logMessage("Fetching Heroes from server....");
+    return this.http.get<Hero[]>(this.apiEndpoint).pipe(
+      tap(_ => this.logMessage("Fetched Heroes from server!")),
+      catchError(this.handleError<Hero[]>("FetchHeroes", []))
+    );
   }
 
-  getHero(hero_id: number) {
-    const hero = HEROES.find(hero => hero.id == hero_id)
-    return of(hero);
+  getHero(id: number) {
+    const endpoint = `${this.apiEndpoint}/${id}`;
+    return this.http.get<Hero>(endpoint).pipe(
+      tap(_ => this.logMessage(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id:${id}`))
+    )
+  }
+
+  httpOptions = {
+    'headers': new HttpHeaders({'Content-Type': 'application/json'})
+  }
+
+  updateHero(hero: Hero) {
+    const url = `${this.apiEndpoint}/${hero.id}`;
+    return this.http.put<Hero>(url, hero, this.httpOptions).pipe(
+      tap(_ => this.logMessage("Updated Hero. ID:" + hero.id)),
+      catchError(this.handleError<Hero>(`UpdateHero: ${hero.id}`))
+    )
+  }
+
+  private handleError<T>(operation: string, results?: T) {
+    return (error?: any) => {
+      let message = error.message || error.statusText;
+      this.logMessage(`${operation} failed: ${message}`);
+      console.log(error)
+      return of(results as T);
+    };
   }
 }
